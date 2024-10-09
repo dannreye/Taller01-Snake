3f0a164059a75525cdfe3cde59d8e98bf3994fe0
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-10-09 13:43:41 -0500

    Integrante 2 - Patiño

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/GoldModel.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/GoldModel.java
@@ -63,7 +63,7 @@ public class GoldModel extends GameModel {
 			
 	/** Graphical representation of the collector */
 	private static final GameTile COLLECTOR_TILE = new RoundTile(Color.BLACK,
-			Color.RED, 2.0);
+			Color.BLUE, 2.0);
 
 	/** Graphical representation of a blank tile. */
 	private static final GameTile BLANK_TILE = new GameTile();
